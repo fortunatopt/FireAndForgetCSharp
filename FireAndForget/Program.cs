9a5113a7ddeed5b1d7b3ccfb0d10976c76f45abf
fortunatopt <patrick_fortunato@yahoo.com>
2018-01-20 18:30:03 -0800

    initial commit

---
+++ FireAndForget/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAndForget
+{
+    class Program
+    {
+        public static void Sum(int sleepTime, int numA, int numB)
+        {
+            System.Threading.Thread.Sleep(sleepTime * 1000);
+            int sum = numA + numB;
+            Console.WriteLine($"The sum of {numA} plus {numB} is {sum}");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter seconds to wait");
+            int sleep = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first value to add");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first value to add");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Task.Factory.StartNew(() => Sum(sleep, num1, num2));
+            Console.WriteLine($"This line is called after the Add() method. You are waiting for {sleep} second{(sleep == 1 ? "" : "s")}");
+            Console.ReadKey();
+        }
+    }
+}
